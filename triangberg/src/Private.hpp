7c143c850ad0c6c9b0e3f09d561a4faf6dd3e941
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-03-12 06:20:26 +0000

    Renamed project

---
+++ triangberg/src/Private.hpp
@@ -0,0 +1,16 @@
+/*
+ * This is a sample private compilation unit.
+ *
+ * <Copyright information goes here>
+ */
+
+#ifndef COM_SAXBOPHONE_TRIANGBERG_PRIVATE_HPP
+#define COM_SAXBOPHONE_TRIANGBERG_PRIVATE_HPP
+
+#include <triangberg/Public.hpp>
+
+namespace com::saxbophone::triangberg::PRIVATE {
+    bool library_works();
+}
+
+#endif // include guard
--- triangberg/src/Public.cpp
+++ triangberg/src/Public.cpp
@@ -4,11 +4,11 @@
  * <Copyright information goes here>
  */
 
-#include <my-hovercraft-is-full-of-eels/Public.hpp>
+#include <triangberg/Public.hpp>
 
 #include "Private.hpp"
 
-namespace com::saxbophone::my_hovercraft_is_full_of_eels {
+namespace com::saxbophone::triangberg {
     bool library_works() {
         return PRIVATE::library_works();
     }
