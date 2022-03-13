77981585cfe3c7d73cf225aeae5738db3456751d
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-03-13 17:20:26 +0000

    Initial implementation of the three ctors for Triangles These allow Triangles to be constructed in the three ways we need them for drawing Triangbergs. The code that will construct Triangles externally will need to check the Triangle does not collide with existing shapes (it'll need access to its lines, something not implemented yet) before calling update_references() on the Triangle to register it with its vertices. That code should try again with the same triangle but requesting a negative angle if it fails (need additional parameter in every ctor to facilitate this). If *that* fails, then it's not possible to put a Triangle in the specified position.

--- triangberg_builder/src/Drawing.cpp
+++ triangberg_builder/src/Drawing.cpp
@@ -6,12 +6,12 @@
 
 #include <cstddef>
 
-#include <array>
 #include <functional>
 #include <memory>
 #include <vector>
 
 #include <triangberg_builder/types.hpp>
+#include <triangberg_builder/geometry.hpp>
 #include <triangberg_builder/Drawing.hpp>
 
 namespace {
@@ -32,39 +32,140 @@ namespace {
     class Triangle; // forward-declaration
     class Vertex {
     public:
-        Vertex(Point position) {}
+        Vertex(Point position) : _position(position) {}
         // Vertex needs to know about every Triangle that uses it
-        void add_triangle(std::shared_ptr<Triangle> triangle) {}
+        void add_triangle(std::shared_ptr<Triangle> triangle) {
+            if (triangle) {
+                this->_triangles.push_back(triangle);
+            }
+        }
+
+        Point get_position() const {
+            return this->_position;
+        }
 
     private:
         Point _position;
         std::vector<std::weak_ptr<Triangle>> _triangles;
     };
 
-    class Triangle {
+    class Triangle : public std::enable_shared_from_this<Triangle> {
     public:
         // ctor for initial triangle
-        Triangle(std::size_t id, Point centre, Degrees orientation, Unit size) {}
+        Triangle(std::size_t id, Point centre, Degrees orientation, Unit size) : _id(id) {
+            // subtend a vertical upwards-pointing line around centre point
+            std::shared_ptr<Vertex> first_vertex = std::make_shared<Vertex>(
+                subtend_point_from_vector(
+                    centre, {0, -size},
+                    degrees_to_radians(orientation)
+                )
+            );
+            this->_vertices.push_back(first_vertex);
+            // subtend another two lines at intervals of 120°
+            for (std::size_t i = 1; i < 3; i++) {
+                std::shared_ptr<Vertex> vertex = std::make_shared<Vertex>(
+                    subtend_point_from_vector(
+                        centre, {0, -size},
+                        degrees_to_radians(orientation + 120 * i)
+                    )
+                );
+                this->_vertices.push_back(vertex);
+            }
+        }
         // ctor for first triangle added (with vertex bound to a point on edge of first triangle)
-        Triangle(std::size_t id, Point first_vertex, Vector first_edge) {}
+        Triangle(std::size_t id, Point first_point, Vector first_edge) : _id(id) {
+            // first vertex is given for us
+            std::shared_ptr<Vertex> first_vertex = std::make_shared<Vertex>(first_point);
+            this->_vertices.push_back(first_vertex);
+            // for the second vertex we just follow the vector of the first edge from the first vertex
+            Point second_point = {
+                first_point.x + first_edge.x, first_point.y + first_edge.y
+            };
+            std::shared_ptr<Vertex> second_vertex = std::make_shared<Vertex>(second_point);
+            this->_vertices.push_back(second_vertex);
+            // for the final vertex we need to subtend the first_edge by 60° around the first_point
+            std::shared_ptr<Vertex> third_vertex = std::make_shared<Vertex>(
+                subtend_point_from_vector(
+                    first_point, first_edge,
+                    degrees_to_radians(60) // TODO: vary sign for collisions
+                )
+            );
+            this->_vertices.push_back(third_vertex);
+        }
         // ctor used for all other triangles, whose vertices are shared with existing triangles
-        Triangle(std::size_t id, std::shared_ptr<Vertex> first, std::shared_ptr<Vertex> second) {}
+        Triangle(std::size_t id, std::shared_ptr<Vertex> first, std::shared_ptr<Vertex> second) : _id(id) {
+            // add the first two vertices
+            this->_vertices.insert(this->_vertices.end(), {first, second});
+            // work out what the vector of the first edge is
+            Point first_point = first->get_position();
+            Point second_point = second->get_position();
+            Vector first_edge = {
+                second_point.x - first_point.x, second_point.y - first_point.y
+            };
+            // subtend this vector about the first point to find the third point
+            std::shared_ptr<Vertex> third_vertex = std::make_shared<Vertex>(
+                subtend_point_from_vector(
+                    first_point, first_edge,
+                    degrees_to_radians(60) // TODO: vary sign for collisions
+                )
+            );
+            this->_vertices.push_back(third_vertex);
+        }
+        // needed to update references to this Triangle from its Vertices (can't be done in ctor)
+        void update_references() {
+            for (auto& vertex : this->_vertices) {
+                vertex->add_triangle(shared_from_this());
+            }
+        }
         // returns the Shape of this Triangle
-        Drawing::Shape get_shape() const;
+        Drawing::Shape get_shape() const {
+            Drawing::Shape shape;
+            for (auto vertex : this->_vertices) {
+                shape.push_back(vertex->get_position());
+            }
+            return shape;
+        }
+        std::shared_ptr<Vertex> get_vertex(std::size_t id) {
+            return this->_vertices[id];
+        }
 
     private:
         std::size_t _id; // tracking identity explicitly is better than pointers
-        std::array<std::shared_ptr<Vertex>, 3> _vertices;
+        std::vector<std::shared_ptr<Vertex>> _vertices;
     };
 }
+
+#include <iostream>
 
 namespace com::saxbophone::triangberg {
     class Drawing::Builder {};
 
-    Drawing::Drawing(Point, Unit, Degrees, EdgeID, Percentage, Degrees)
-      : _stage(0)
+    Drawing::Drawing(
+        Point origin,
+        Unit size,
+        Degrees rotation,
+        EdgeID branch_edge,
+        Percentage branch_point,
+        Degrees branch_angle
+    ) : _stage(0)
       , _builder(new Builder())
-      {}
+      {
+        std::shared_ptr<Triangle> t = std::make_shared<Triangle>(0, origin, rotation, size);
+        t->update_references();
+        for (auto point : t->get_shape()) {
+            std::cout << point.x << ", " << point.y << std::endl;
+        }
+        std::shared_ptr<Triangle> u = std::make_shared<Triangle>(1, Point{400, 305}, Vector{10, 10});
+        u->update_references();
+        for (auto point : u->get_shape()) {
+            std::cout << point.x << ", " << point.y << std::endl;
+        }
+        std::shared_ptr<Triangle> v = std::make_shared<Triangle>(2, t->get_vertex(1), u->get_vertex(1));
+        v->update_references();
+        for (auto point : v->get_shape()) {
+            std::cout << point.x << ", " << point.y << std::endl;
+        }
+    }
 
     Drawing::~Drawing() = default;
 
